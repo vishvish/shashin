7820f4c3ce42c6efe1be9708768b5aca20bc74ad
Vish Vishvanath <vishvish@users.noreply.github.com>
2020-12-20 22:37:21 +0000

    Setting up parallel tests and mime-type checking.
    Signed-off-by: Vish Vishvanath <vishvish@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -28,10 +28,13 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem "mime-types"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-    gem 'rspec-rails', '~> 4.0.0.beta'
+  gem 'rspec-rails', '~> 4.0.0.beta'
+  gem 'parallel_tests'
 end
 
 group :development do
---
+++ spec/models/inspector_canon_1D_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Inspector, type: :model do
+  let(:inspector) { described_class.new('spec/example-files/') }
+  let(:canon_1dx_file) { '_G4R6399.CR2' }
+
+  describe 'reading Files' do
+    context 'with a Canon 1DX file' do
+      let(:inspected) { inspector.read(canon_1dx_file) }
+
+      it 'reads the filename' do
+        expect(inspected.filename).to eq('_G4R6399.CR2')
+      end
+
+      it 'reads the created_datetime' do
+        expect(inspected.created).to eq('2017:06:25 11:45:10')
+      end
+
+      it 'reads the camera make' do
+        expect(inspected.camera_manufacturer).to eq('Canon')
+      end
+
+      it 'reads the camera model' do
+        expect(inspected.camera_model).to eq('Canon EOS-1D X')
+      end
+
+      it 'returns the directory structure based on the timestamp' do
+        expect(inspected.directory_structure).to eq("2017/06/25")
+      end
+    end
+  end
+end
